5e25c7dac1541ef6aec16df8ee1eec3cb1d04574
Joshua Nobleza <joshnobleza@bell.net>
2022-05-30 14:16:33 -0400

    completed the myMap function

--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -16,8 +16,16 @@ let result2 = myMap(['run', 'Forrest'], function (el) {
 console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 *******************************************************************************/
 
-let myMap = function() {
+let myMap = function (arr, cb) {
+    let newArr = [];
 
+    for (let i = 0; i < arr.length; i++) {
+        let el = arr[i];
+        let res = cb(el, i, arr);
+        newArr.push(res);
+    }
+
+    return newArr;
 };
 
 
