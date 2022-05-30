daac87bf49d28bf34306053d4a25228b2229554c
Joshua Nobleza <joshnobleza@bell.net>
2022-05-30 16:17:38 -0400

    completed mySome function

--- problems/07-my-some.js
+++ problems/07-my-some.js
@@ -23,8 +23,17 @@ let result3 = mySome(['soup', 'noodles', 'bike', 'ship'], function(ele) {
 console.log(result3);   // true
 *******************************************************************************/
 
-let mySome = function() {
+let mySome = function (arr, cb) {
+    for (let i = 0; i < arr.length; i++) {
+        let el = arr[i];
+        let resIsTrue = cb(el, i);
 
+        if (resIsTrue) {
+            return true;
+        }
+    }
+
+    return false;
 };
 
 
