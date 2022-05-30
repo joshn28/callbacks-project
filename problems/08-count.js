129c6babc985b0ccda216d1a30995dd76f10963f
Joshua Nobleza <joshnobleza@bell.net>
2022-05-30 16:21:26 -0400

    completed count function

--- problems/08-count.js
+++ problems/08-count.js
@@ -26,8 +26,16 @@ let result4 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str)
 console.log(result4); // 0
 *******************************************************************************/
 
-let count = function() {
+let count = function (arr, cb) {
+    let counter = 0;
+    arr.forEach(function (el) {
+        let resIsTrue = cb(el);
+        if (resIsTrue) {
+            counter++;
+        }
+    });
 
+    return counter;
 };
 
 
